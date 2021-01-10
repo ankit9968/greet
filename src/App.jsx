9d3bee1158263753b2a97d6bd108c45578a84f0c
Ankit Yadav <yadav.abhi0210@gmail.com>
2021-01-10 13:19:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./index.css";
+
+let currdate= new Date();
+currdate=currdate.getHours();
+let greeting="";
+const cssstyle={};
+if(currdate>=0 && currdate<12)
+   {
+   	greeting="Good Morning";
+   	cssstyle.color="green";
+   }
+     else if(currdate>=12 && currdate<18){
+     	greeting="Good Afternoon";
+     	cssstyle.color="orange";
+
+     }
+     else
+     {
+     	greeting="Good Night";
+     	cssstyle.color="blue";
+     }
+     const  App=()=>{
+       return(
+       <div>
+  <h1>Hello Sir Ankit Yadav is Greetting you <span style={cssstyle}>{greeting}</span></h1>
+  </div>
+  );}
+  export default App;
